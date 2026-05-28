25a403482f8038c19f75b86d90943312ccce0456
Mamun123123123 <almalm90459@gmail.com>
2026-05-28 22:45:08 +0800

    secondLargest

--- indexs.js
+++ indexs.js
@@ -10,3 +10,14 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(7));
+
+
+function secondLargest(arr) {
+  let uniqueArr = [...new Set(arr)];
+
+  uniqueArr.sort((a, b) => b - a);
+
+  return uniqueArr[1];
+}
+
+console.log(secondLargest([10, 5, 8, 20, 15]));
