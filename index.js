5e7c4eb264e4c72f00dbfa52820c42bc67440b47
Mamun123123123 <almalm90459@gmail.com>
2026-06-08 18:24:33 +0600

    second largest

--- index.js
+++ index.js
@@ -27,3 +27,24 @@ function removeDuplicates(arr) {
 
 console.log(removeDuplicates([1,2,2,3,4,4,5]));
 
+
+
+function secondLargest(arr) {
+    if (arr.length < 2) return null;
+
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
+
+    for (let num of arr) {
+        // Skip duplicates of largest
+        if (num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } 
+        else if (num > secondLargest && num < largest) {
+            secondLargest = num;
+        }
+    }
+
+    return secondLargest === -Infinity ? null : secondLargest;
+}
