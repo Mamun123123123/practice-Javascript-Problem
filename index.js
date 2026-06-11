076e9662667b58e313f1fddb768e3ad8e2cb5e3d
Mamun123123123 <almalm90459@gmail.com>
2026-06-11 22:23:58 +0600

    missing number

--- index.js
+++ index.js
@@ -95,3 +95,20 @@ function firstNonRepeatingChar(str) {
 
 console.log(firstNonRepeatingChar("aabbcdde")); // c
 console.log(firstNonRepeatingChar("aabbcc"));   // null
+
+
+
+
+
+function missingNumber(arr, n) {
+    let total = (n * (n + 1)) / 2;
+
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return total - sum;
+}
+
+console.log(missingNumber([1, 2, 4, 5, 6], 6)); // 3
